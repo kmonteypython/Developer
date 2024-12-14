452c9cd54cf4434482f02297fdb304b086a03dd6
kmonteypython <kevin.montesdeoca@outlook.com>
2024-12-14 00:33:23 -0700

    Create New Request

--- createNewVariables.js
+++ createNewVariables.js
@@ -3,7 +3,7 @@ var variables = [
     {name: 'variable1', type: '1', question: 'Enter your name'},
     {name: 'variable2', type: '2', question: 'Select your role'}
 ];
-
+//
 variables.forEach(function(v) {
     var varGR = new GlideRecord('item_option_new');
     varGR.initialize();
@@ -17,3 +17,24 @@ variables.forEach(function(v) {
 '1' == 'Yes/No';
 '2' == 'Multli Line Text';
 '5' == 'Select Box';
+
+function createServiceRequest() {
+    var cart = new Cart();
+    var item = cart.addItem('catalog_item_sys_id');
+    
+    // Set variables for the request
+    cart.setVariable(item, 'short_description', 'Automated Service Request');
+    cart.setVariable(item, 'description', 'This request was created automatically');
+    
+    // Set the requested for user
+    var cartGR = cart.getCart();
+    cartGR.requested_for = 'user_sys_id';
+    cartGR.update();
+    
+    // Place the order
+    var newSerReq = cart.placeOrder();
+    
+    // Additional updates if needed
+    newSerReq.opened_by = 'creator_sys_id';
+    newSerReq.update();
+  }
